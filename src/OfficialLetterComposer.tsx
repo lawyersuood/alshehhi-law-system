4db52828ef298f959e88e7d900cb2043048e9ed6
lawyersuood <lawyersuood@gmail.com>
2026-08-30 10:30:23 +0400

    refactor: migrate to react-quill-new and add html2pdf
    Migrate Quill editor to the maintained react-quill-new package and integrate html2pdf.js to support PDF export functionality.

--- src/OfficialLetterComposer.tsx
+++ src/OfficialLetterComposer.tsx
@@ -10,8 +10,8 @@ import {
   ImageIcon,
   Trash2,
 } from "lucide-react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+import ReactQuill from "react-quill-new";
+import "react-quill-new/dist/quill.snow.css";
 
 const LETTERHEAD_LAYOUT = {
   headerMm: 46,
